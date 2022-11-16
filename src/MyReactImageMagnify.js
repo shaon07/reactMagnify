298001d78e4075510dc4f695ffa0994a2ee0f948
md shamirul islam <alishaon078@gmail.com>
2022-11-16 05:01:18 +0000

    completed

--- src/MyReactImageMagnify.js
+++ src/MyReactImageMagnify.js
@@ -1,39 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 
 import ReactImageMagnify from "react-image-magnify";
-import { withStyles } from "@material-ui/core/styles";
 
-const styles = (theme) => ({
-  root: {}
-});
+const MyReactImageMagnify = () => {
+  return (
+    <ReactImageMagnify
+      {...{
+        smallImage: {
+          alt: "Wristwatch by Ted Baker London",
+          isFluidWidth: true,
+          src: require("./cover.png"),
+        },
+        largeImage: {
+          src: require("./cover.png"),
+          width: 640,
+          height: 480,
+        },
+        enlargedImageContainerStyle: {
+          zIndex: "1500",
+        },
+        enlargedImageContainerDimensions: {
+          width: "100%",
+          height: "100%",
+        },
+      }}
+    />
+  );
+};
 
-class MyReactImageMagnify extends Component {
-  render() {
-    return (
-      <ReactImageMagnify
-        {...this.props}
-        {...{
-          smallImage: {
-            alt: "Wristwatch by Ted Baker London",
-            isFluidWidth: true,
-            src: "https://placeimg.com/640/480/any"
-          },
-          largeImage: {
-            src: "https://placeimg.com/640/480/any",
-            width: 640,
-            height: 480
-          },
-          enlargedImageContainerStyle: {
-            zIndex: "1500"
-          },
-          enlargedImageContainerDimensions: {
-            width: "100%",
-            height: "100%"
-          }
-        }}
-      />
-    );
-  }
-}
-
-export default withStyles(styles)(MyReactImageMagnify);
+export default MyReactImageMagnify;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import Grid from "@material-ui/core/Grid";
-import { Paper } from "@material-ui/core";
-
-import "react-image-gallery/styles/css/image-gallery.css";
 
 import MyReactImageMagnify from "./MyReactImageMagnify";
 
-class App extends Component {
-  render() {
-    return (
-      <Grid container spacing={4}>
-        <Grid item xs={6}>
-          <MyReactImageMagnify />
-        </Grid>
-        <Grid container spacing={2} item xs={6} direction="column">
-          <Grid item></Grid>
-        </Grid>
+const App = () => {
+  return (
+    <Grid container spacing={4}>
+      <Grid item xs={6}>
+        <MyReactImageMagnify />
       </Grid>
-    );
-  }
-}
+      <Grid container spacing={2} item xs={6} direction="column">
+        <Grid item></Grid>
+      </Grid>
+    </Grid>
+  );
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
